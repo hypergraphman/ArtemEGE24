b82469424a504f8e6be737d9babfcbd9e46ad91d
Руслан Аитов <r_aitov@mail.ru>
2023-02-02 19:47:55 +0300

    task5

---
+++ task8/r10.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+s = set()
+for word in permutations('kapkan'):
+    word = ''.join(word)
+    if 'kk' not in word and 'aa' not in word:
+        s.add(word)
+print(len(s))
---
+++ task8/r11.py
@@ -0,0 +1,8 @@
+from itertools import product
+
+c = 0
+for word in product('SKOLA', repeat=3):
+    word = ''.join(word)
+    if word.count('K') == 1:
+        c += 1
+print(c)
